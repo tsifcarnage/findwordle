6dd7d9fcf1f8f7eccbb65e2e2419468d9111a3a0
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2025-12-10 22:10:59 +0100

    nav
    nav qui contient logo titre toggle dark

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
-<script setup></script>
+<script>
+import Nav from "./components/Nav.vue";
+export default {
+  components: {
+    Nav,
+  },
+};
+</script>
 
 <template>
+  <Nav />
   <h1 class="text-info text-6xl">Find Wordle</h1>
 </template>
 
